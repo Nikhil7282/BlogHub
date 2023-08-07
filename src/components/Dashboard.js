71b32fcf3384dc72925eb5c607af2a2e179678cc
NIKHIL <112640546+Nikhil7282@users.noreply.github.com>
2023-08-07 18:22:01 +0530

    Like Feature

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,58 +1,106 @@
-import React,{useState,useEffect} from 'react'
-import { Container, Card,Button } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Container, Card } from "react-bootstrap";
 import { url } from "../App";
-import {useNavigate} from 'react-router-dom'
-import axios from 'axios'
-import { toast } from 'react-toastify';
-import {AiFillLike,AiFillDislike} from 'react-icons/ai'
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { AiFillLike, AiFillDislike } from "react-icons/ai";
+
 
 function Dashboard() {
-  const Navigate=useNavigate()
-  const [blogData,setBlogData]=useState([])
+  
+  const Navigate = useNavigate();
+  const [blogData, setBlogData] = useState([]);
 
-  const logout =()=>{
-    sessionStorage.clear()
-    Navigate('/login')
-  }
-  useEffect(()=>{
-    if(sessionStorage.getItem('token')){
-      fetchData()
+  const logout = () => {
+    sessionStorage.clear();
+    Navigate("/login");
+  };
+  
+  useEffect(() => {
+    if (sessionStorage.getItem("token")) {
+      fetchData();
+    } else {
+      logout();
     }
-    else{
-      logout()
-    }
-  },[])
+  }, []);
 
-  const fetchData= async()=>{
+
+  //Likes
+  const handleLike=async(id,userId)=>{
     try {
-      const res=await axios.get(`${url}/blogs`)
-      setBlogData(res.data)
+      const post=await blogData.find((post)=>post._id===id)
+      // console.log(post)
+      const index=await post.likes.findIndex((user)=>user===userId)
+      // console.log(index)
+      // console.log(id)
+      // console.log(userId);
+      if(index===-1){
+        await axios.post(`${url}/blogs/likePost/${id}`,{
+          headers:{
+            Authorization:`Bearer ${sessionStorage.getItem("token")}`,
+          },
+        })
+        .then((res)=>{
+          if(res){
+            post.likes.push(userId)
+          }
+        })
+      }
+      else{
+        await axios.post(`${url}/blogs/unLikePost/${id}`,{
+          headers:{
+            Authorization:`Bearer ${sessionStorage.getItem("token")}`
+          }
+        })
+        post.likes.splice(index,1);
+      }
+      setBlogData([...blogData])
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const fetchData = async () => {
+    try {
+      const res = await axios.get(`${url}/blogs`);
+      setBlogData(res.data);
       // console.log(res.data)
     } catch (error) {
-      if(error.response.status ===401 ||error.response.status ===400){
-        logout()
-        toast.error(error.response.data.message)
+      if (error.response.status === 401 || error.response.status === 400) {
+        logout();
+        toast.error(error.response.data.message);
       }
       // console.log(error)
     }
-  }
+  };
 
   const getRandomColor = () => {
-    const colors = ['Primary','Secondary','Success','Danger','Warning','Info','Dark'];
+    const colors = [
+      "Primary",
+      "Secondary",
+      "Success",
+      "Danger",
+      "Warning",
+      "Info",
+      "Dark",
+    ];
     const randomIndex = Math.floor(Math.random() * colors.length);
     return colors[randomIndex];
   };
 
   return (
     <div>
-       <Container className="text-center mt-5">
+      <Container className="text-center mt-5">
         <div className="d-flex flex-wrap justify-content-center mt-5">
           {blogData.map((card, index) => (
-            <Card 
-            bg={getRandomColor().toLowerCase()}
-            key={index} 
-            style={{ width: "18rem", margin: "10px"}}
-            text={getRandomColor().toLowerCase() === 'light' ? 'dark' : 'white'}
+            <Card
+              bg={getRandomColor().toLowerCase()}
+              key={index}
+              style={{ width: "18rem", margin: "10px" }}
+              text={
+                getRandomColor().toLowerCase() === "light" ? "dark" : "white"
+              }
             >
               <Card.Body>
                 <Card.Title>{card.title}</Card.Title>
@@ -60,21 +108,29 @@ function Dashboard() {
                 <Card.Text>{card.content}</Card.Text>
               </Card.Body>
               <Card.Footer>
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-          <div>{new Date(card.createdAt).toLocaleDateString()}</div>
-          {/* <Button variant="primary">Like</Button> */}
-          <div style={{display:'flex',alignItems:'center'}}>
-          <AiFillLike/>
-          <span style={{ marginLeft: '5px' }}>{0} Likes</span>
-          </div>
-        </div>
-      </Card.Footer>
+                <div
+                  style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  <div>{new Date(card.createdAt).toLocaleDateString()}</div>
+                  {/* <Button variant="primary">Like</Button> */}
+                  <div style={{ display: "flex", alignItems: "center" }}>
+                    {card.likes.some(
+                      (userId) => userId === sessionStorage.getItem("userId")
+                    ) ? (
+                      <AiFillDislike onClick={()=>{handleLike(card._id,sessionStorage.getItem("userId"))}}/>
+                    ) : (
+                      <AiFillLike onClick={()=>{handleLike(card._id,sessionStorage.getItem("userId"))}} />
+                    )}
+                    <span style={{ marginLeft: "5px" }}>{card.likes.length} Likes</span>
+                  </div>
+                </div>
+              </Card.Footer>
             </Card>
           ))}
         </div>
       </Container>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
