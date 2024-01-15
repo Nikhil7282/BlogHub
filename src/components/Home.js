e9d9d93d2f57ae4b2368fceb340ea8ef64118780
Nikhil7282 <nikhil.sa2020ece@sece.ac.in>
2024-01-15 12:28:54 +0530

    ref

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,27 @@
-import React,{useContext} from "react";
-import { Container,Card } from "react-bootstrap";
+import React, { useContext } from "react";
+import { Container, Card } from "react-bootstrap";
 import { postContext } from "../context/globalContext.js";
 import NewNavbar from "./NewNavbar.js";
 import Loader from "./Loader.js";
 
-
-function Home(){
-  const {state}=useContext(postContext)
+function Home() {
+  const { state } = useContext(postContext);
   const getRandomColor = () => {
-    const colors = ['Primary','Secondary','Success','Danger','Warning','Info','Dark'];
+    const colors = [
+      "Primary",
+      "Secondary",
+      "Success",
+      "Danger",
+      "Warning",
+      "Info",
+      "Dark",
+    ];
     const randomIndex = Math.floor(Math.random() * colors.length);
     return colors[randomIndex];
   };
 
-  if(state.loading===true){
-    return <Loader/>
+  if (state.loading === true) {
+    return <Loader />;
   }
   return (
     <div
@@ -25,17 +32,19 @@ function Home(){
         minHeight: "100vh",
       }}
     >
-      <NewNavbar state="Home"/>
+      <NewNavbar state="Home" />
       <Container className="text-center mt-5">
-        <h1>Welcome to BlogHub</h1>
-        <h5>Register to create your first blog.</h5>
+        <h1>Beyond Words Elevate Your Thoughts at BlogHub</h1>
+        <h5>Register and create your first blog.</h5>
         <div className="d-flex flex-wrap justify-content-center mt-5">
           {state.data.map((card) => (
-            <Card 
-            bg={getRandomColor().toLowerCase()} 
-            key={card._id} 
-            style={{ width: "18rem", margin: "10px"}}
-            text={getRandomColor().toLowerCase() === 'light' ? 'dark' : 'white'}
+            <Card
+              bg={getRandomColor().toLowerCase()}
+              key={card._id}
+              style={{ width: "18rem", margin: "10px" }}
+              text={
+                getRandomColor().toLowerCase() === "light" ? "dark" : "white"
+              }
             >
               <Card.Body>
                 <Card.Title>{card.title}</Card.Title>
@@ -48,6 +57,6 @@ function Home(){
       </Container>
     </div>
   );
-};
+}
 
 export default Home;
