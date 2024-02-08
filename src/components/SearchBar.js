42cc21f8b587b616ff03db3f4a56be1014e3f0bf
Nikhil7282 <nikhilsabbz@gmail.com>
2024-02-08 20:30:27 +0530

    search

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,22 @@
 import React, { useContext, useState } from "react";
 import { IoSearch } from "react-icons/io5";
 import { postContext } from "../context/globalContext";
+import { useNavigate } from "react-router-dom";
 
 const SearchBar = () => {
   const { state } = useContext(postContext);
   const [results, setResults] = useState([]);
-  // console.log(state);
+  const [showSearchResults, setShowSearchResults] = useState(false);
+  const navigate = useNavigate();
+  window.addEventListener("click", (e) => {
+    if (
+      e.target.className !== "result-title" ||
+      e.target.className !== "result-list-item"
+    ) {
+      setShowSearchResults(false);
+    } else return;
+  });
+
   const handleChange = (e) => {
     const filteredPost = state.data.filter((post) => {
       return (
@@ -13,15 +24,40 @@ const SearchBar = () => {
       );
     });
     setResults(filteredPost);
-    console.log(results);
+    setShowSearchResults(true);
   };
+
   const handleClick = () => {};
+
+  const handleList = (postId, post) => {
+    // console.log(postId);
+    setShowSearchResults(false);
+    navigate(`/user/postPage/${postId}`, { state: { card: post } });
+  };
   return (
     <div className="searchBar">
       <input type="text" placeholder="Search" onChange={handleChange} />
       <button onClick={handleClick}>
         <IoSearch />
       </button>
+      {showSearchResults && results.length > 0 ? (
+        <div className="results-list">
+          {results.map((post) => (
+            <div
+              key={post._id}
+              className="result-list-item"
+              onClick={() => {
+                handleList(post._id, post);
+              }}
+            >
+              <p className="result-title">{post.title}</p>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <></>
+        // <div className="results-list">No results Found</div>
+      )}
     </div>
   );
 };
